0a8ca16ed103f8d380e40fff29be9f8d0f3aebee
JPVindas <jvindas40886@ufide.ac.cr>
2024-12-16 18:21:24 -0600

    Shopping Cart

---
+++ Tienda/Libreria/Controllers/CarritoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace TuProyecto.Controllers
+{
+    public class CarritoController : Controller
+    {
+        // Acción para mostrar la vista del carrito
+        public ActionResult IndexCart()
+        {
+            // Aquí puedes enviar datos adicionales si los necesitas
+            return View();
+        }
+    }
+}
